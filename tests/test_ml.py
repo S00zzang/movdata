69de21c30978bfdb075f6579301c2b8062c4f77a
S00zzang <sujinpark8719561@gmail.com>
2024-08-20 16:21:18 +0900

    test fin

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -1,6 +1,6 @@
 from movdata.ml import save_movies
 from movdata.get_mov_detail import load_movie_list, make_movie_info_data
+from movdata.get_mov_companies import load_movie_companies
 
-# 연도별 저장된 영화들의 상세 정보 데이터 저장
-def test_load_movies():
-    assert make_movie_info_data(2015)
+def test_load_companies():
+    assert load_movie_companies()
